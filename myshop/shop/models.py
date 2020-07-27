56cf75e8251ec033327711e14fec8815b98ce703
Osei Tutu Amoabin <otamoabin2@gmail.com>
2020-07-27 13:50:01 -0500

    sessions set on cart

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -52,31 +52,31 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse("shop:product_detail", args=[self.id, self.slug])
 
-    class Cart(models.Model):
-        cart_id  = models.CharField(max_length=250, blank= True)
-        date_added =models.DateField(auto_now_add = True)
+    # class Cart(models.Model):
+    #     cart_id  = models.CharField(max_length=250, blank= True)
+    #     date_added =models.DateField(auto_now_add = True)
         
         
-        class Meta:
-            db_table = 'Cart'
-            ordering = ['date_added']
+    #     class Meta:
+    #         db_table = 'Cart'
+    #         ordering = ['date_added']
             
-        def __str__(self):
-            return self.cart_id
+    #     def __str__(self):
+    #         return self.cart_id
         
         
-    class CartItem(models.Model):
-        product = models.ForeignKey('shop.Product', on_delete=models.CASCADE)
-        cart = models.ForeignKey('shop.Cart', on_delete=models.CASCADE)
-        quantity = models.IntegerField()
-        active = models.BooleanField(default=True)
+    # class CartItem(models.Model):
+    #     product = models.ForeignKey('shop.Product', on_delete=models.CASCADE)
+    #     cart = models.ForeignKey('shop.Cart', on_delete=models.CASCADE)
+    #     quantity = models.IntegerField()
+    #     active = models.BooleanField(default=True)
         
         
-        class Meta:
-            db_table = 'CartItem'
+    #     class Meta:
+    #         db_table = 'CartItem'
             
-        def sub_total(self):
-            return self.product.price * self.quantity
+    #     def sub_total(self):
+    #         return self.product.price * self.quantity
         
-        def __str__(self):
-            return self.product
+    #     def __str__(self):
+    #         return self.product
--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('about/', views.about_us, name='about_us'),
     path('category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-     path('cart/',),
+    #  path('cart/',),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
     path('create-account/', CreateAccount.as_view(), name="create_account"),
 
